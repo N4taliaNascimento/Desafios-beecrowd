6c5749023b226cc40cfb0107e93c8b4ff0b8cebd
N4taliaNascimento <nataliasanntos161@gmail.com>
2025-05-14 00:20:25 -0300

    1006.py
    Média 2

---
+++ python/1006.py
@@ -0,0 +1,12 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+pesoa = 2.0
+pesob = 3.0
+pesoc = 5.0
+
+somapesos = pesoa + pesob + pesoc
+media = (a*pesoa + b*pesob + c*pesoc)/ somapesos
+
+print(f'MEDIA = {media:.1f}')
